3cbe37e353035f5738b3413fa1a114379efbfa3f
PGPradhan <pradhan.prathamesh10@gmail.com>
2024-03-02 19:33:47 +0530

    third commit

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -2,6 +2,7 @@ import "./HeroImageStyles.css";
 import IntroImg from '../assests/intro-bg.jpg';
 import React from 'react'
 import { Link } from "react-router-dom";
+import Typewriter from "./Typewriter";
 
 const HeroImage = () => {
   return (
@@ -10,8 +11,12 @@ const HeroImage = () => {
     <img className="intro-img" src={IntroImg} alt="IntroImg"/>
     </div>
     <div className="content">
+        
         <p>HI I'M FREELANCER</p>
-        <h1>React Developer.</h1>
+        <h1 >
+        <Typewriter text="React Developer" delay={100} infinite />
+        </h1>
+     
         <div>
              <Link to="/project" className="btn">Projects</Link>
             <Link to='/contact' className="btn btn-light">Contact</Link>
